c6ff0d1b9fcb78449db7359514be51c4e163c2bc
Polatov Doniyor <polatovdoniyor@Polatovs-MacBook-Pro.local>
2026-06-13 13:03:40 +0500

    fix order conv states

--- bot.py
+++ bot.py
@@ -21,26 +21,28 @@ def main():
 
     app.add_handler(CommandHandler("start", start_handler.start))
 
-    # Orders - to'liq ConversationHandler ICHIDA
     order_conv = ConversationHandler(
         entry_points=[CallbackQueryHandler(orders_handler.orders_menu, pattern="^orders$")],
         states={
             1: [CallbackQueryHandler(orders_handler.order_select_shop, pattern="^ord_shop_")],
             2: [CallbackQueryHandler(orders_handler.order_select_product, pattern="^ord_prod_")],
-            3: [MessageHandler(filters.TEXT & ~filters.COMMAND, orders_handler.order_text_handler)],
-            4: [CallbackQueryHandler(orders_handler.order_discount_type, pattern="^disc_"),
-                MessageHandler(filters.TEXT & ~filters.COMMAND, orders_handler.order_text_handler)],
-            5: [MessageHandler(filters.TEXT & ~filters.COMMAND, orders_handler.order_text_handler)],
-            6: [MessageHandler(filters.TEXT & ~filters.COMMAND, orders_handler.order_text_handler)],
-            7: [CallbackQueryHandler(orders_handler.order_save, pattern="^ord_save$"),
-                CallbackQueryHandler(start_handler.back_to_main, pattern="^main_menu$")],
+            3: [MessageHandler(filters.TEXT & ~filters.COMMAND, orders_handler.order_enter_qty)],
+            4: [
+                CallbackQueryHandler(orders_handler.order_discount_type, pattern="^disc_"),
+                MessageHandler(filters.TEXT & ~filters.COMMAND, orders_handler.order_discount_value),
+            ],
+            5: [MessageHandler(filters.TEXT & ~filters.COMMAND, orders_handler.order_enter_paid)],
+            6: [MessageHandler(filters.TEXT & ~filters.COMMAND, orders_handler.order_enter_note)],
+            7: [
+                CallbackQueryHandler(orders_handler.order_save, pattern="^ord_save$"),
+                CallbackQueryHandler(start_handler.back_to_main, pattern="^main_menu$"),
+            ],
         },
         fallbacks=[CallbackQueryHandler(start_handler.back_to_main, pattern="^main_menu$")],
         allow_reentry=True
     )
     app.add_handler(order_conv)
 
-    # Auth
     auth_conv = ConversationHandler(
         entry_points=[
             CallbackQueryHandler(auth_handler.register_start, pattern="^auth_register$"),
@@ -52,7 +54,6 @@ def main():
     )
     app.add_handler(auth_conv)
 
-    # Shops
     shop_conv = ConversationHandler(
         entry_points=[CallbackQueryHandler(shops_handler.shops_menu, pattern="^shops$")],
         states=shops_handler.STATES,
@@ -61,7 +62,6 @@ def main():
     )
     app.add_handler(shop_conv)
 
-    # Products
     product_conv = ConversationHandler(
         entry_points=[CallbackQueryHandler(products_handler.products_menu, pattern="^products$")],
         states=products_handler.STATES,
@@ -70,7 +70,6 @@ def main():
     )
     app.add_handler(product_conv)
 
-    # Debtors
     debtor_conv = ConversationHandler(
         entry_points=[CallbackQueryHandler(debtors_handler.debtors_menu, pattern="^debtors$")],
         states=debtors_handler.STATES,
@@ -79,7 +78,6 @@ def main():
     )
     app.add_handler(debtor_conv)
 
-    # History
     history_conv = ConversationHandler(
         entry_points=[CallbackQueryHandler(history_handler.history_menu, pattern="^history$")],
         states=history_handler.STATES,
